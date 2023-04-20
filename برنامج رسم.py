5bf2c6a4d34e19b438c42aff7fb681021d8c58a1
thedarknight1996 <118132241+thedarknight1996@users.noreply.github.com>
2023-04-20 12:55:13 -0700

    Update برنامج رسم.py

--- برنامج رسم.py
+++ برنامج رسم.py
@@ -8,18 +8,18 @@ wi.resizable(False,False)
 wi.attributes("-alpha",0.7)
 wi.config(background="black")
 
-
 def ass():
-    if d1.get()=="مثلث":
+    if d1.get()=="triangle":
         dd2=int(d2.get()) #قياس
         dd3=int(d3.get()) #انحراف
         dd4=int(d4.get()) #تكرار
+        dd5=str(d5.get()) #اللون
         fd=turtle.Screen()
-        fd.setup(500,500,250,250)
+        fd.setup(500,500,dd2-250,dd2-250)
         fd.bgcolor("black")
         ff=turtle.Turtle()
         ff.speed(5)
-        ff.color("blue")
+        ff.color(dd5)
         for m in range(dd4):
             for i in range(3):
                 ff.forward(dd2)
@@ -27,16 +27,17 @@ def ass():
             ff.right(dd3)
         fd.mainloop()
 
-    elif d1.get()=="مربع":
+    elif d1.get()=="Square":
         dd2=int(d2.get()) #قياس
         dd3=int(d3.get()) #انحراف
         dd4=int(d4.get()) #تكرار
+        dd5=str(d5.get()) #اللون
         fd=turtle.Screen()
-        fd.setup(500,500,250,250)
+        fd.setup(500,500,dd2-250,dd2-250)
         fd.bgcolor("black")
         ff=turtle.Turtle()
         ff.speed(5)
-        ff.color("blue")
+        ff.color(dd5)
         for m in range(dd4):
             for i in range(4):
                 ff.forward(dd2)
@@ -44,43 +45,52 @@ def ass():
             ff.right(dd3)
         fd.mainloop()       
 
-    elif d1.get()=="دائرة":
+    elif d1.get()=="circle":
         dd2=int(d2.get()) #قياس
         dd3=int(d3.get()) #انحراف
         dd4=int(d4.get()) #تكرار
+        dd5=str(d5.get()) #اللون
         fd=turtle.Screen()
-        fd.setup(500,500,250,250)
+        fd.setup(500,500,dd2-250,dd2-250)
         fd.bgcolor("black")
         ff=turtle.Turtle()
         ff.speed(5)
-        ff.color("blue")
+        ff.color(dd5)
         for m in range(dd4):
             ff.circle(dd2)
             ff.right(dd3)
         fd.mainloop()       
 
 
-s1=Label(wi,text="ماهو الشكل الذي تريده ان يرسم",fg="white",font=(15),bg="black",justify="center")
-s1.place(y=20,x=270)
-d1=ttk.Combobox(wi,value=("مثلث","مربع","دائرة"),state="readonly",font=("arial",10),width=22,justify="center")
-d1.place(y=22,x=20)
+s1=Label(wi,text="What shape do you want to draw ?",fg="white",font=(15),bg="black",justify="center")
+s1.place(y=20,x=20)
+d1=ttk.Combobox(wi,value=("triangle","Square","circle"),state="readonly",font=("arial",10),width=15,justify="center")
+d1.place(y=22,x=350)
 
-s2=Label(wi,text="ماهو قياس الشكل المرسوم",fg="white",font=(10),bg="black")
-s2.place(y=70,x=305)
+s2=Label(wi,text="What is the size of the drawn figure ?",fg="white",font=(10),bg="black")
+s2.place(y=70,x=20)
 d2=Entry(wi,font=(10),width=14,justify="center")
-d2.place(y=72,x=20)
+d2.place(y=72,x=350)
 
-s3=Label(wi,text="ماهي نسبة الانحراف التي تريدها",fg="white",font=(10),bg="black")
-s3.place(y=120,x=265)
+s3=Label(wi,text="What angle of deviation do you want ?",fg="white",font=(10),bg="black")
+s3.place(y=120,x=20)
 d3=Entry(wi,font=(10),width=14,justify="center")
-d3.place(y=122,x=20)
+d3.place(y=122,x=350)
 
-s4=Label(wi,text="ما عدد التكرار الذي تريده",fg="white",font=(10),bg="black")
-s4.place(y=170,x=310)
+s4=Label(wi,text="How many repetitions do you want ?",fg="white",font=(10),bg="black")
+s4.place(y=170,x=20)
 d4=Entry(wi,font=(10),width=14,justify="center")
-d4.place(y=172,x=20)
+d4.place(y=172,x=350)
 
-b=Button(wi,text="ارسم",font=(20),command=ass)
-b.place(y=300,x=230)
+s5=Label(wi,text="What color do you want the painting to be ?",fg="white",font=(15),bg="black",justify="center")
+s5.place(y=220,x=20)
+d5=ttk.Combobox(wi,value=("white","purple","green","red","blue","yellow","orange","grey"),state="readonly",font=("arial",10),width=15,justify="center")
+d5.place(y=222,x=350)
+
+b=Button(wi,text="drow",font=(25),fg="green",command=ass)
+b.place(y=350,x=230)
+
+c=Button(wi,text="X\n closs the brogram",font=(20),fg="red",command=wi.quit)
+c.place(y=400,x=180)
 
 wi.mainloop()
